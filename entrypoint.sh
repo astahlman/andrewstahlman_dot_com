631e31579872e69cb4845728ffcbf1bdb218627f
Andrew Stahlman <andrew.stahlman@gmail.com>
2020-01-25 22:10:40 -0800

    Try relative path

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,3 +10,10 @@ export PATH="$PATH:$CASK"
 
 cask install
 /root/.cask/bin/cask emacs --batch -l bootstrap-build.el -f "build-website"
+
+### Diagnostics
+stat build/
+echo "Github workspace is: ${GITHUB_WORKSPACE}"
+stat "${GITHUB_WORKSPACE}"
+cp -r build/ "${GITHUB_WORKSPACE}"
+
